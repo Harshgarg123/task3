7c2fbebe60f5a5ef449d69651a22c829ef058706
Harshgarg123 <142335665+Harshgarg123@users.noreply.github.com>
2025-08-02 23:30:52 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input("Enter number"))
+
+
+result = math.factorial(num)
+
+print(f"Factorial of {num} is: {result}")
+
+
---
+++ task2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)  
+sine_val = math.sin(num)  
+
+print(f"Square root of {num} = {sqrt_val}")
+print(f"Natural logarithm of {num} = {log_val}")
+print(f"Sine of {num} radians = {sine_val}")
